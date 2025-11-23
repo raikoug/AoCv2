0ab865e4d03cdde82163bacdc2a53f0a09814edc
Raikoug <raikoug@gmail.com>
2025-11-23 20:22:14 +0100

    first rust

---
+++ rust/crates/y2024/src/bin/day_02.rs
@@ -0,0 +1,20 @@
+use aoclib::read_input;
+
+const YEAR: i32 = 2024;
+const DAY: u8 = 2;
+
+fn part1(_input: &str) -> i64 {
+    // TODO: implementa la logica della parte 1
+    0
+}
+
+fn part2(_input: &str) -> i64 {
+    // TODO: implementa la logica della parte 2
+    0
+}
+
+fn main() {
+    let input = read_input(YEAR, DAY, 1).expect("cannot read input");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
