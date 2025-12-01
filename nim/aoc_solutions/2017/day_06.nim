0296c39d642fba2de582a1d420907afd474fc2d8
Raikoug <raikoug@gmail.com>
2025-12-01 22:55:27 +0100

    2017, day 6, NIM

---
+++ nim/aoc_solutions/2017/day_06.nim
@@ -0,0 +1,61 @@
+import std/strutils
+import aoclib/aoc_input
+import sequtils
+
+const
+  Year* = 2017
+  Day*  = 6
+
+proc part1*(input: string): int =
+
+  var registers: seq[int] = input.strip().splitWhitespace().map(parseInt)
+  var steps = 0
+  var history: seq[seq[int]] = @[]
+
+  history.add(registers)
+
+  while true:
+    var index = maxIndex(registers)
+    var val = registers[index]
+    registers[index] = 0
+    for _ in 1..val:
+      index = (index + 1) mod len(registers)
+      registers[index] += 1
+    
+    steps += 1
+    if history.contains(registers):
+      return steps
+
+    history.add(registers)
+    
+
+proc part2*(input: string): int =
+
+  var registers: seq[int] = input.strip().splitWhitespace().map(parseInt)
+  var steps = 0
+  var history: seq[seq[int]] = @[]
+
+  history.add(registers)
+
+  while true:
+    var index = maxIndex(registers)
+    var val = registers[index]
+    registers[index] = 0
+    for _ in 1..val:
+      index = (index + 1) mod len(registers)
+      registers[index] += 1
+    
+    steps += 1
+    if history.contains(registers):
+      break
+
+    history.add(registers)
+  
+  let first_index = findIt(history, it == registers)
+  steps - first_index
+
+when isMainModule:
+  let input = readInput(Year, Day)
+  let test = "0 2 7 0"
+  echo "Part 1: ", part1(input)
+  echo "Part 2: ", part2(input)
